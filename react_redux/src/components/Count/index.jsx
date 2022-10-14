b9a60cb8041e9e6a1ec57166bfb372289c76d331
suzhenyi <suzhenyi1999@163.com>
2022-10-14 23:52:15 +0800

    react-redux

--- react_redux/src/components/Count/index.jsx
+++ react_redux/src/components/Count/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import store from '../../redux/store'
+// import store from '../../redux/store'
 import { createDecrementAction,createIncrementAction,createIncrementAsyncAction } from '../../redux/count_action'
 
 export default class Count extends Component {
@@ -7,31 +7,32 @@ export default class Count extends Component {
   increment = () => {
     const {value} = this.selectNumber
     // store.dispatch({type:'increment', data: value*1})
-    store.dispatch(createIncrementAction(value*1))
+    // store.dispatch(createIncrementAction(value*1))
+    this.props.jia(value*1)
+
   }
 
   decrement = () => {
     const {value} = this.selectNumber
     // store.dispatch({type:'decrement', data: value*1})
-    store.dispatch(createDecrementAction(value*1))
+    // store.dispatch(createDecrementAction(value*1))
+    this.props.jian(value*1)
   }
 
   incrementIfOdd = () => {
     // const {value} = this.selectNumber
-
   }
 
   incrementAsync = () => {
     const {value} = this.selectNumber
-    store.dispatch(createIncrementAsyncAction(value*1, 1000))
-
+    // store.dispatch(createIncrementAsyncAction(value*1, 1000))
+    this.props.jiaAsync(value*1, 1000)
   }
-
 
   render() {
     return (
       <div>
-        <h1>当前求和为{store.getState()}</h1>
+        <h1>当前求和为{this.props.count}</h1>
         <select ref={ c => this.selectNumber = c}>
           <option value="1">1</option>
           <option value="2">2</option>
---
+++ react_redux/src/containers/Count/index.jsx
@@ -0,0 +1,23 @@
+import {connect} from 'react-redux'
+
+import CountUI from '../../components/Count'
+
+import { 
+  createDecrementAction,
+  createIncrementAction,
+  createIncrementAsyncAction 
+} from '../../redux/count_action'
+
+function mapStateToProps(state) {
+  return {count: state}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    jia: number => dispatch(createIncrementAction(number)),
+    jian: number => dispatch(createDecrementAction(number)),
+    jiaAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CountUI)

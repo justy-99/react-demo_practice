3c09415dfb3281f64e1c3e9f7e31e5e48edb3ea9
suzhenyi <suzhenyi1999@163.com>
2022-10-15 18:19:02 +0800

    容器和UI组件整合

--- react_redux/src/containers/Count/index.jsx
+++ react_redux/src/containers/Count/index.jsx
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux'
+import React, { Component } from 'react'
 
-import CountUI from '../../components/Count'
+// import CountUI from '../../components/Count'
 
 import { 
   createDecrementAction,
@@ -15,6 +16,40 @@ import {
 //   decrement: number => dispatch(createDecrementAction(number)),
 //   incrementAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time))
 // })
+class CountUI extends Component {
+
+  increment = () => {
+    const {value} = this.selectNumber
+    this.props.increment(value*1)
+  }
+
+  decrement = () => {
+    const {value} = this.selectNumber
+    this.props.decrement(value*1)
+  }
+
+  incrementAsync = () => {
+    const {value} = this.selectNumber
+    this.props.incrementAsync(value*1, 1000)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>当前求和为{this.props.count}</h1>
+        <select ref={ c => this.selectNumber = c}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>&nbsp;
+
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.incrementAsync}>异步加</button>
+      </div>
+    )
+  }
+}
 
 
 export default connect(

ee1a26a824a3c792321593dbea4ec77ef221b980
suzhenyi <suzhenyi1999@163.com>
2022-11-15 21:50:59 +0800

    rc

--- react_css/src/index.js
+++ react_css/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./views/02/App";
+import App from "./views/03/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
